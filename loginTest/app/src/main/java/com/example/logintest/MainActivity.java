ff1849b047f36d36d506f6cf78ea4fb481bbd583
Park-new-project <parkmj236@gmail.com>
2019-09-30 21:46:38 +0900

    loginTest 빌드3 - 오류 발생 signin 결과물이 계속 false를 반환

--- loginTest/app/src/main/java/com/example/logintest/MainActivity.java
+++ loginTest/app/src/main/java/com/example/logintest/MainActivity.java
@@ -9,9 +9,12 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
@@ -19,7 +22,7 @@ import com.google.android.gms.tasks.Task;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
-    private static final int RC_SIGN_IN = 9001;
+    private int RC_SIGN_IN = 1000;
     com.google.android.gms.auth.api.signin.GoogleSignInClient mGoogleSignInClient;
 
     @Override
@@ -51,37 +54,64 @@ public class MainActivity extends AppCompatActivity {
         //updateUI(account);
     }
 
-    private void handleSignInResult(Task<GoogleSignInAccount> task) {
-        Log.d("@@@@ ", "정보 받아오는 중1");
-        try{
-            // 에러 발생
-            GoogleSignInAccount account = task.getResult(ApiException.class);
+//    private void handleSignInResult(Task<GoogleSignInAccount> task) {
+//        Log.d("@@@@ ", "handleSignInResult 진입");
+//        try{
+//            // 에러 발생
+//            GoogleSignInAccount account = task.getResult(ApiException.class);
+//
+//            Log.d("@@@@ ", "정보 받아오는 중2");
+//            String email = account.getEmail();
+//            String name1 = account.getFamilyName();
+//            String name2 = account.getGivenName();
+//            String name3 = account.getDisplayName();
+//
+//            Log.d("@@@@", email);
+//            Log.d("@@@@", name1);
+//            Log.d("@@@@", name2);
+//            Log.d("@@@@", name3);
+//        }
+//        catch (ApiException e) {
+//            Log.d("@@@@ ", "ApiException 발생"+e.getStatusCode());
+//            Toast.makeText(this, "ApiException 발생" + e.getStatusCode(), Toast.LENGTH_SHORT).show();
+//            e.printStackTrace();
+//        }
+//    }
 
-            Log.d("@@@@ ", "정보 받아오는 중2");
-            String email = account.getEmail();
-            String name1 = account.getFamilyName();
-            String name2 = account.getGivenName();
-            String name3 = account.getDisplayName();
+    private void handleSignInResult(GoogleSignInResult result)
+    {
+        Log.d("@@@@", "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess())
+        {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
 
-            Log.d("@@@@", email);
-            Log.d("@@@@", name1);
-            Log.d("@@@@", name2);
-            Log.d("@@@@", name3);
+            Log.d("@@@@", "" + acct.getDisplayName());
+            Log.d("@@@@", "" + acct.getEmail());
+            Log.d("@@@@", "" + acct.getPhotoUrl());
+            Log.d("@@@@", "" + acct.getId());
         }
-        catch (ApiException e) {
-            Log.d("@@@@ ", "ApiException 발생");
-            Toast.makeText(this, "ApiException 발생" + e.getStatusCode(), Toast.LENGTH_SHORT).show();
-            e.printStackTrace();
+        else
+        {
+            // Signed out, show unauthenticated UI.
         }
     }
-    @Override
+
+
+        @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
         if(requestCode ==RC_SIGN_IN){
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            Log.d("@@@@ ", "handleSignInResult() 실행");
-            handleSignInResult(task);
+//            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+//            Log.d("@@@@ ", "handleSignInResult() 실행");
+//
+//            Log.d("@@@@ ", "task : "+task);
+//            handleSignInResult(task);
+
+            Log.d("@@@@", "onActivityResult:RC_SIGN_IN : " + RC_SIGN_IN);
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
         }
     }
 
@@ -90,7 +120,6 @@ public class MainActivity extends AppCompatActivity {
         //RC_SIGN_IN : 임의의 수로 이루어진 요청코드로 활동완료시 리턴
         Log.d("@@@@ ", "startActivityForResult() 실행");
         startActivityForResult(signInIntent, RC_SIGN_IN);   //로그인 창열기
-
     }
 
 }
