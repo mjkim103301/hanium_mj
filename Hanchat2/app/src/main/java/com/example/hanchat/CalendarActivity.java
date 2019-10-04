58e44d253e7002159110ac6641eb2d44b85ad73f
Park-new-project <parkmj236@gmail.com>
2019-10-04 20:54:07 +0900

    한챗

--- Hanchat2/app/src/main/java/com/example/hanchat/CalendarActivity.java
+++ Hanchat2/app/src/main/java/com/example/hanchat/CalendarActivity.java
@@ -60,6 +60,7 @@ public class CalendarActivity extends NavActivity
 
     Button bt_go_chat;
     Intent intent;
+    Intent intent_profile;
     String TAG = "@@@@ ";
 
     // Google Calendar API 객체
@@ -89,6 +90,7 @@ public class CalendarActivity extends NavActivity
         setContentView(R.layout.activity_calendar);
 
         intent = new Intent(CalendarActivity.this, MainActivity.class);
+        intent_profile = new Intent(CalendarActivity.this, ProfileActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
 
         // 우측 상단 버튼 (캘린더 화면으로 이동)
@@ -168,6 +170,15 @@ public class CalendarActivity extends NavActivity
         bt_go_chat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                // 네비게이션 헤더 클릭시 프로필로 이동
+                headerview.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        startActivity(intent_profile);
+                    }
+                });
+
                 Intent intent = new Intent(CalendarActivity.this, MainActivity.class);
                 // 동일한 activity가 stack에 연속적으로 쌓였을때 activity를 재사용 | 새 태스크를 만드는 플래그
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
@@ -180,42 +191,37 @@ public class CalendarActivity extends NavActivity
 
 
 
-    /**
-     * 다음 사전 조건을 모두 만족해야 Google Calendar API를 사용할 수 있다.
-     *
-     * 사전 조건
-     *     - Google Play Services 설치
-     *     - 유효한 구글 계정 선택
-     *     - 안드로이드 디바이스에서 인터넷 사용 가능
-     *
-     * 하나라도 만족하지 않으면 해당 사항을 사용자에게 알림.
-     */
+//     다음 사전 조건을 모두 만족해야 Google Calendar API를 사용 가능
+//     1. Google Play Services 설치                         isGooglePlayServicesAvailable()
+//     2. 유효한 구글 계정 선택                             mCredential.getSelectedAccountName() == null
+//     3. 안드로이드 디바이스에서 인터넷 사용 가능          isDeviceOnline()
     private void getResultsFromApi() {
 
         Log.d(TAG, "!sGooglePlayServicesAvailable() : "+ !isGooglePlayServicesAvailable());
         Log.d(TAG, "mCredential.getSelectedAccountName() == null : "+ (mCredential.getSelectedAccountName() == null));
         Log.d(TAG, "!isDeviceOnline() : "+ !isDeviceOnline());
 
-        if (!isGooglePlayServicesAvailable()) { // Google Play Services를 사용할 수 없는 경우
+
+        // Google Play Services를 사용할 수 없는 경우
+        if (!isGooglePlayServicesAvailable()) {
             acquireGooglePlayServices();
-        } else if (mCredential.getSelectedAccountName() == null) { // 유효한 Google 계정이 선택되어 있지 않은 경우
-
+        }
+        // 유효한 Google 계정이 선택되어 있지 않은 경우
+        else if (mCredential.getSelectedAccountName() == null) {
             chooseAccount();
-        } else if (!isDeviceOnline()) {    // 인터넷을 사용할 수 없는 경우
-
-            mStatusText.setText("No network connection available.");
-        } else {
-
-            // Google Calendar API 호출
+        }
+        // 인터넷을 사용할 수 없는 경우
+        else if (!isDeviceOnline()) {
+            mStatusText.setText("인터넷 연결을 확인하세요.");
+        }
+        // 위에 모두 해당하지 않을 경우에 Google Calendar API 호출
+        else {
             new MakeRequestTask(this, mCredential).execute();
         }
+
     }
 
-
-
-    /**
-     * 안드로이드 디바이스에 최신 버전의 Google Play Services가 설치되어 있는지 확인
-     */
+    // 안드로이드 디바이스에 최신 버전의 Google Play Services가 설치되어 있는지 확인
     private boolean isGooglePlayServicesAvailable() {
 
         GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
@@ -224,50 +230,10 @@ public class CalendarActivity extends NavActivity
         return connectionStatusCode == ConnectionResult.SUCCESS;
     }
 
-
-
-    /*
-     * Google Play Services 업데이트로 해결가능하다면 사용자가 최신 버전으로 업데이트하도록 유도하기위해
-     * 대화상자를 보여줌.
-     */
-    private void acquireGooglePlayServices() {
-
-        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
-        final int connectionStatusCode = apiAvailability.isGooglePlayServicesAvailable(this);
-
-        if (apiAvailability.isUserResolvableError(connectionStatusCode)) {
-
-            showGooglePlayServicesAvailabilityErrorDialog(connectionStatusCode);
-        }
-    }
-
-
-
-    /*
-     * 안드로이드 디바이스에 Google Play Services가 설치 안되어 있거나 오래된 버전인 경우 보여주는 대화상자
-     */
-    void showGooglePlayServicesAvailabilityErrorDialog(
-            final int connectionStatusCode
-    ) {
-
-        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
-
-        Dialog dialog = apiAvailability.getErrorDialog(
-                CalendarActivity.this,
-                connectionStatusCode,
-                REQUEST_GOOGLE_PLAY_SERVICES
-        );
-        dialog.show();
-    }
-
-
-
-    /*
-     * Google Calendar API의 자격 증명( credentials ) 에 사용할 구글 계정을 설정한다.
-     *
-     * 전에 사용자가 구글 계정을 선택한 적이 없다면 다이얼로그에서 사용자를 선택하도록 한다.
-     * GET_ACCOUNTS 퍼미션이 필요하다.
-     */
+    // 구글 계정 선택 다이얼로그
+    //Google Calendar API의 자격 증명(Credential) 에 사용할 구글 계정을 설정한다.
+    // 전에 사용자가 구글 계정을 선택한 적이 없다면 다이얼로그에서 사용자를 선택하도록 한다.
+    // GET_ACCOUNTS 퍼미션이 필요하다.
     @AfterPermissionGranted(REQUEST_PERMISSION_GET_ACCOUNTS)
     private void chooseAccount() {
 
@@ -307,99 +273,7 @@ public class CalendarActivity extends NavActivity
         }
     }
 
-
-
-    /*
-     * 구글 플레이 서비스 업데이트 다이얼로그, 구글 계정 선택 다이얼로그, 인증 다이얼로그에서 되돌아올때 호출된다.
-     */
-
-    @Override
-    protected void onActivityResult(
-            int requestCode,  // onActivityResult가 호출되었을 때 요청 코드로 요청을 구분
-            int resultCode,   // 요청에 대한 결과 코드
-            Intent data
-    ) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-
-        switch (requestCode) {
-
-            case REQUEST_GOOGLE_PLAY_SERVICES:
-
-                if (resultCode != RESULT_OK) {
-
-                    mStatusText.setText( " 앱을 실행시키려면 구글 플레이 서비스가 필요합니다."
-                            + "구글 플레이 서비스를 설치 후 다시 실행하세요." );
-                } else {
-
-                    getResultsFromApi();
-                }
-                break;
-
-
-            case REQUEST_ACCOUNT_PICKER:
-                if (resultCode == RESULT_OK && data != null && data.getExtras() != null) {
-                    String accountName = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
-                    if (accountName != null) {
-                        SharedPreferences settings = getPreferences(Context.MODE_PRIVATE);
-                        SharedPreferences.Editor editor = settings.edit();
-                        editor.putString(PREF_ACCOUNT_NAME, accountName);
-                        editor.apply();
-                        mCredential.setSelectedAccountName(accountName);
-                        getResultsFromApi();
-                    }
-                }
-                break;
-
-
-            case REQUEST_AUTHORIZATION:
-
-                if (resultCode == RESULT_OK) {
-                    getResultsFromApi();
-                }
-                break;
-        }
-    }
-
-
-    /*
-     * Android 6.0 (API 23) 이상에서 런타임 권한 요청시 결과를 리턴받음
-     */
-    @Override
-    public void onRequestPermissionsResult(
-            int requestCode,  //requestPermissions(android.app.Activity, String, int, String[])에서 전달된 요청 코드
-            @NonNull String[] permissions, // 요청한 퍼미션
-            @NonNull int[] grantResults    // 퍼미션 처리 결과. PERMISSION_GRANTED 또는 PERMISSION_DENIED
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
-    }
-
-
-    /*
-     * EasyPermissions 라이브러리를 사용하여 요청한 권한을 사용자가 승인한 경우 호출된다.
-     */
-    @Override
-    public void onPermissionsGranted(int requestCode, List<String> requestPermissionList) {
-
-        // 아무일도 하지 않음
-    }
-
-
-    /*
-     * EasyPermissions 라이브러리를 사용하여 요청한 권한을 사용자가 거부한 경우 호출된다.
-     */
-    @Override
-    public void onPermissionsDenied(int requestCode, List<String> requestPermissionList) {
-
-        // 아무일도 하지 않음
-    }
-
-
-    /*
-     * 안드로이드 디바이스가 인터넷 연결되어 있는지 확인한다. 연결되어 있다면 True 리턴, 아니면 False 리턴
-     */
+    // 안드로이드 디바이스가 인터넷 연결되어 있는지 확인한다. 연결되어 있다면 True 리턴, 아니면 False 리턴
     private boolean isDeviceOnline() {
 
         ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
@@ -407,42 +281,6 @@ public class CalendarActivity extends NavActivity
 
         return (networkInfo != null && networkInfo.isConnected());
     }
-
-
-    /*
-     * 캘린더 이름에 대응하는 캘린더 ID를 리턴
-     */
-    private String getCalendarID(String calendarTitle){
-
-        String id = null;
-
-        // Iterate through entries in calendar list
-        String pageToken = null;
-        do {
-            CalendarList calendarList = null;
-            try {
-                calendarList = mService.calendarList().list().setPageToken(pageToken).execute();
-            } catch (UserRecoverableAuthIOException e) {
-                startActivityForResult(e.getIntent(), REQUEST_AUTHORIZATION);
-            }catch (IOException e) {
-                e.printStackTrace();
-            }
-            List<CalendarListEntry> items = calendarList.getItems();
-
-
-            for (CalendarListEntry calendarListEntry : items) {
-
-                if ( calendarListEntry.getSummary().toString().equals(calendarTitle)) {
-
-                    id = calendarListEntry.getId().toString();
-                }
-            }
-            pageToken = ((CalendarList) calendarList).getNextPageToken();
-        } while (pageToken != null);
-
-        return id;
-    }
-
 
     // 비동기적으로 Google Calendar API 호출
     private class MakeRequestTask extends AsyncTask<Void, Void, String> {
@@ -453,7 +291,6 @@ public class CalendarActivity extends NavActivity
 
 
         private MakeRequestTask(CalendarActivity activity, GoogleAccountCredential credential) {
-
             mActivity = activity;
 
             HttpTransport transport = AndroidHttp.newCompatibleTransport();
@@ -465,56 +302,40 @@ public class CalendarActivity extends NavActivity
                     .build();
         }
 
-
+        // doInBackground()의 준비작업
         @Override
         protected void onPreExecute() {
-            // mStatusText.setText("");
             mProgress.show();
             mStatusText.setText("데이터 가져오는 중...");
             mResultText.setText("");
         }
 
-
-        /*
-         * 백그라운드에서 Google Calendar API 호출 처리
-         */
+        // 백그라운드에서 Google Calendar API 호출 처리 실행
         @Override
         protected String doInBackground(Void... params) {
             try {
-
                 if ( mID == 1) {
-
                     return createCalendar();
-
-                }else if (mID == 2) {
-
+                }
+                else if (mID == 2) {
                     return addEvent();
                 }
                 else if (mID == 3) {
-
                     return getEvent();
                 }
-
-
             } catch (Exception e) {
                 mLastError = e;
                 cancel(true);
                 return null;
             }
-
             return null;
         }
 
-
-        /*
-         * CalendarTitle 이름의 캘린더에서 10개의 이벤트를 가져와 리턴
-         */
+        // CalendarTitle 이름의 캘린더에서 10개의 이벤트를 가져와 리턴
         private String getEvent() throws IOException {
-
-
             DateTime now = new DateTime(System.currentTimeMillis());
 
-            String calendarID = getCalendarID("CalendarTitle");
+            String calendarID = getCalendarID("Hanchat");
             if ( calendarID == null ){
 
                 return "캘린더를 먼저 생성하세요.";
@@ -531,14 +352,13 @@ public class CalendarActivity extends NavActivity
 
 
             for (Event event : items) {
-
                 DateTime start = event.getStart().getDateTime();
                 if (start == null) {
 
-                    // 모든 이벤트가 시작 시간을 갖고 있지는 않다. 그런 경우 시작 날짜만 사용
+                    // 모든 이벤트가 시작 시간을 갖고 있지는 않음
+                    // 그런 경우 시작 날짜만 사용
                     start = event.getStart().getDate();
                 }
-
 
                 eventStrings.add(String.format("%s \n (%s)", event.getSummary(), start));
             }
@@ -547,12 +367,10 @@ public class CalendarActivity extends NavActivity
             return eventStrings.size() + "개의 데이터를 가져왔습니다.";
         }
 
-        /*
-         * 선택되어 있는 Google 계정에 새 캘린더를 추가한다.
-         */
+        // 선택되어 있는 Google 계정에 새 캘린더를 추가한다.
         private String createCalendar() throws IOException {
 
-            String ids = getCalendarID("CalendarTitle");
+            String ids = getCalendarID("Hanchat");
 
             if ( ids != null ){
 
@@ -563,7 +381,7 @@ public class CalendarActivity extends NavActivity
             com.google.api.services.calendar.model.Calendar calendar = new Calendar();
 
             // 캘린더의 제목 설정
-            calendar.setSummary("CalendarTitle");
+            calendar.setSummary("Hanchat");
 
 
             // 캘린더의 시간대 설정
@@ -628,7 +446,7 @@ public class CalendarActivity extends NavActivity
         private String addEvent() {
 
 
-            String calendarID = getCalendarID("CalendarTitle");
+            String calendarID = getCalendarID("Hanchat");
 
             if ( calendarID == null ){
 
@@ -637,18 +455,16 @@ public class CalendarActivity extends NavActivity
             }
 
             Event event = new Event()
-                    .setSummary("구글 캘린더 테스트")
-                    .setLocation("서울시")
-                    .setDescription("캘린더에 이벤트 추가하는 것을 테스트합니다.");
+                    .setSummary("구글 캘린더 테스트")                                  //#### 캘린더의 이름
+                    .setLocation("서울시")                                             //#### 캘린더의 위치
+                    .setDescription("캘린더에 이벤트 추가하는 것을 테스트합니다.");    //#### 캘린더의 메모
 
 
             java.util.Calendar calander;
 
             calander = java.util.Calendar.getInstance();
             SimpleDateFormat simpledateformat;
-            //simpledateformat = new SimpleDateFormat( "yyyy-MM-dd'T'HH:mm:ssZ", Locale.KOREA);
-            // Z에 대응하여 +0900이 입력되어 문제 생겨 수작업으로 입력
-            simpledateformat = new SimpleDateFormat( "yyyy-MM-dd'T'HH:mm:ss+09:00", Locale.KOREA);
+            simpledateformat = new SimpleDateFormat( "yyyy-MM-dd'T'HH:mm:ss+09:00", Locale.KOREA);    //#### 캘린더의 날짜 시간데이터
             String datetime = simpledateformat.format(calander.getTime());
 
             DateTime startDateTime = new DateTime(datetime);
@@ -683,5 +499,142 @@ public class CalendarActivity extends NavActivity
         }
     }
 
+
+
+    // Google Play Services 최신버전 업데이트 유도
+    private void acquireGooglePlayServices() {
+
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+        final int connectionStatusCode = apiAvailability.isGooglePlayServicesAvailable(this);
+
+        if (apiAvailability.isUserResolvableError(connectionStatusCode)) {
+
+            showGooglePlayServicesAvailabilityErrorDialog(connectionStatusCode);
+        }
+    }
+
+    // 서비스 업데이트 다이얼로그
+    void showGooglePlayServicesAvailabilityErrorDialog(
+            final int connectionStatusCode
+    ) {
+
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+
+        Dialog dialog = apiAvailability.getErrorDialog(
+                CalendarActivity.this,
+                connectionStatusCode,
+                REQUEST_GOOGLE_PLAY_SERVICES
+        );
+        dialog.show();
+    }
+
+
+
+    //구글 플레이 서비스 업데이트 다이얼로그, 구글 계정 선택 다이얼로그, 인증 다이얼로그에서 되돌아올때 호출된다.
+    @Override
+    protected void onActivityResult(
+            int requestCode,  // onActivityResult가 호출되었을 때 요청 코드로 요청을 구분
+            int resultCode,   // 요청에 대한 결과 코드
+            Intent data
+    ) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        switch (requestCode) {
+
+            case REQUEST_GOOGLE_PLAY_SERVICES:
+
+                if (resultCode != RESULT_OK) {
+
+                    mStatusText.setText( " 앱을 실행시키려면 구글 플레이 서비스가 필요합니다."
+                            + "구글 플레이 서비스를 설치 후 다시 실행하세요." );
+                } else {
+
+                    getResultsFromApi();
+                }
+                break;
+
+
+            case REQUEST_ACCOUNT_PICKER:
+                if (resultCode == RESULT_OK && data != null && data.getExtras() != null) {
+                    String accountName = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
+                    if (accountName != null) {
+                        SharedPreferences settings = getPreferences(Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = settings.edit();
+                        editor.putString(PREF_ACCOUNT_NAME, accountName);
+                        editor.apply();
+                        mCredential.setSelectedAccountName(accountName);
+                        getResultsFromApi();
+                    }
+                }
+                break;
+
+
+            case REQUEST_AUTHORIZATION:
+
+                if (resultCode == RESULT_OK) {
+                    getResultsFromApi();
+                }
+                break;
+        }
+    }
+    
+    // Android 6.0 (API 23) 이상에서 런타임 권한 요청시 결과를 리턴받음
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode,  //requestPermissions(android.app.Activity, String, int, String[])에서 전달된 요청 코드
+            @NonNull String[] permissions, // 요청한 퍼미션
+            @NonNull int[] grantResults    // 퍼미션 처리 결과. PERMISSION_GRANTED 또는 PERMISSION_DENIED
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+
+
+    // EasyPermissions 라이브러리를 사용하여 요청한 권한을 사용자가 승인한 경우 호출된다.
+    @Override
+    public void onPermissionsGranted(int requestCode, List<String> requestPermissionList) {
+        // 아무일도 하지 않음
+    }
+
+    // EasyPermissions 라이브러리를 사용하여 요청한 권한을 사용자가 거부한 경우 호출된다.
+    @Override
+    public void onPermissionsDenied(int requestCode, List<String> requestPermissionList) {
+        // 아무일도 하지 않음
+    }
+
+
+    // 캘린더 이름에 대응하는 캘린더 ID를 리턴
+    private String getCalendarID(String calendarTitle){
+
+        String id = null;
+
+        // Iterate through entries in calendar list
+        String pageToken = null;
+        do {
+            CalendarList calendarList = null;
+            try {
+                calendarList = mService.calendarList().list().setPageToken(pageToken).execute();
+            } catch (UserRecoverableAuthIOException e) {
+                startActivityForResult(e.getIntent(), REQUEST_AUTHORIZATION);
+            }catch (IOException e) {
+                e.printStackTrace();
+            }
+            List<CalendarListEntry> items = calendarList.getItems();
+
+
+            for (CalendarListEntry calendarListEntry : items) {
+
+                if ( calendarListEntry.getSummary().toString().equals(calendarTitle)) {
+
+                    id = calendarListEntry.getId().toString();
+                }
+            }
+            pageToken = ((CalendarList) calendarList).getNextPageToken();
+        } while (pageToken != null);
+
+        return id;
+    }
 
 }
